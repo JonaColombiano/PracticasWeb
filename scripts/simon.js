8cb6c47d6401f199614f99c4748ecbf00e2fa147
Jonathan Alberto <jona_chivas_4000@hotmail.com>
2019-05-31 01:57:37 -0500

    Acance simondice.js

--- scripts/simon.js
+++ scripts/simon.js
@@ -1,6 +1,31 @@
-function getElementkey(keycode){
-    const letra = document.addEventListener("keydown", function(ev){
-        console.log(ev)    
-    })
-    
+const niveles = 15
+let key = generarTeclas(niveles)
+
+function generarTeclas(niveles){
+    return new Array(niveles).fill(0).map(generarTeclaAleatoria)
+}
+
+function generarTeclaAleatoria(){
+    const min = 65
+    const max = 90
+    return Math.round(Math.random() * (max-min)+min)
+}
+
+function getElementByKeyCode(keyCode){
+    return document.querySelector(`[data-key="${keyCode}"]`);
+}
+
+function active(keyCode, opts={}){
+    const el = getElementByKeyCode(keyCode)
+    el.classList.add('active')
+    if(opts.sucess){
+       el.classList.add('sucess')
+       }else if (opts.fail){
+           el.classList.add('fail')
+       }
+    setTimeout(()=>desactive(el),500)
+}
+
+function desactive (el){
+    el.className = 'letra'
 }
