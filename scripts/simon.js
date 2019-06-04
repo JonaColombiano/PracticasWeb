7fd329d956bae791359d72047d63272672d2dd53
Jonathan Alberto <jona_chivas_4000@hotmail.com>
2019-06-04 17:03:32 -0500

    Juego terminado Simindice.js

--- scripts/simon.js
+++ scripts/simon.js
@@ -1,5 +1,56 @@
 const niveles = 15
-let key = generarTeclas(niveles)
+let teclas = generarTeclas(niveles)
+
+function siguienteNivel(nivelActual){
+    if(nivelActual==niveles){
+        return Swal.fire({
+            title:'ganaste',
+            type:'success',
+        })
+    }
+    Swal.fire({
+        timer:800,
+        title:`Nivel ${nivelActual+1}`,
+        showConfirmButton:false,
+    })
+    
+    for(let i=0; i<=nivelActual; i++){
+        setTimeout(()=>active(teclas[i]),1000*i+1500)
+    }
+    
+    let i=0
+    let teclaActual=teclas[i]
+    window.addEventListener('keydown', onkeydown)
+    
+    function onkeydown(ev){
+        if(ev.keyCode==teclaActual){
+            active(teclaActual, {success:true})
+           i++
+            if(i>nivelActual){
+               window.removeEventListener('keydown', onkeydown)
+                setTimeout(()=>siguienteNivel(i),1500)
+               }
+            teclaActual=teclas[i]
+           }else{
+                active(ev.keyCode, {fail:true});
+                window.removeEventListener('keydown', onkeydown);
+                Swal.fire({
+                    title:'Perdiste',
+                    text:'¿Quieres Jugar de Nuevo?'
+                }).then((result)=>{
+                    if(result.value){
+                        teclas=generarTeclas(niveles)
+                        siguienteNivel(0)
+                    }
+                })
+
+                }
+            }
+     }
+    
+
+
+siguienteNivel(0)
 
 function generarTeclas(niveles){
     return new Array(niveles).fill(0).map(generarTeclaAleatoria)
@@ -8,7 +59,7 @@ function generarTeclas(niveles){
 function generarTeclaAleatoria(){
     const min = 65
     const max = 90
-    return Math.round(Math.random() * (max-min)+min)
+    return Math.round(Math.random() * (max-min)+ min )
 }
 
 function getElementByKeyCode(keyCode){
@@ -18,8 +69,8 @@ function getElementByKeyCode(keyCode){
 function active(keyCode, opts={}){
     const el = getElementByKeyCode(keyCode)
     el.classList.add('active')
-    if(opts.sucess){
-       el.classList.add('sucess')
+    if(opts.success){
+       el.classList.add('success')
        }else if (opts.fail){
            el.classList.add('fail')
        }
